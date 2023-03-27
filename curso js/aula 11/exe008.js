415fbfb6af467d707cac3d42c4e7806ef8222700
Matheus Mendes <114121856+matheusmendesde@users.noreply.github.com>
2023-03-27 17:41:09 -0300

    JAVAA

---
+++ curso js/aula 11/exe008.js
@@ -0,0 +1,9 @@
+var vel = 70.5
+console.log(`A velocidade do seu carro é ${vel}KM/H `)
+
+if (vel < 70) {
+    console.log('Esta na velocidade correta, dirija sempre usando cinto de segurança!')
+} 
+console.log('Você ultrapassou o limite MULTADO')
+
+
---
+++ curso js/aula 11/exe009.js
@@ -0,0 +1,8 @@
+var pais = 'brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil' || 'brasil') {
+    console.log('Você é brasileiro samba futebol ihull')
+}
+else {
+    console.log('Você é estrangeiro')
+}
